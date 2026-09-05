0b6a9a2c0ee0c9f53a6ca350ecf745ec2b206313
srivastavakhushi57 <srivkhushi57@gmail.com>
2026-09-05 16:18:42 +0530

    Odd Even Checker

---
+++ 13. Odd or Even/script.js
@@ -0,0 +1,21 @@
+let num1=5;
+let num2=10;
+let result1;
+let result2;
+
+if(num1%2==0){
+    result1="Even.";
+}
+else{
+    result1="Odd.";
+}
+
+if(num2%2==0){
+    result2="Even.";
+}
+else{
+    result2="Odd.";
+}
+
+console.log("The number",num1,"is",result1);
+console.log("The number",num2,"is",result2);
